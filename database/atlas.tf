bfa752b5815775b8bbfe499f515754aef1503443
Chris Bosman <christopher.r.bosman@gmail.com>
2019-05-03 11:11:49 -0500

    changes

--- database/atlas.tf
+++
@@ -1,9 +0,0 @@
-variable "atlas_username" {}
-variable "atlas_api_key" {}
-
-provider "mongodbatlas" {
-    username    = "${var.atlas_username}"
-    api_key     = "${var.atlas_api_key}"
-}
-
-resource "mongodbatlas_cluster" "deployment" {}
---
+++ infrastructure/database/atlas.tf
@@ -0,0 +1,36 @@
+variable "atlas_username" {}
+variable "atlas_api_key" {}
+variable "atlas_org_id" {}
+variable "atlas_org_name" {}
+variable "atlas_provider" {}
+variable "atlas_region" {}
+
+provider "mongodbatlas" {
+    username    = "${var.atlas_username}"
+    api_key     = "${var.atlas_api_key}"
+}
+
+resource "mongodbatlas_project" "deployment" {
+    org_id  = "${var.atlas_org_id}"
+    name    = "${var.atlas_org_name}"
+}
+
+resource "mongodbatlas_container" "deployment" {
+    group               = "${mongodbatlas_project.deployment.id}"
+    atlas_cidr_block    = ""
+    provider_name       = "${var.atlas_provider}"
+    region              = "${var.atlas_region}"
+}
+
+resource "mongodbatlas_cluster" "deployment" {
+    name                    = "${var.atlas_org_name}"
+    group                   = "${mongodbatlas_project.deployment.id}"
+    mongodb_major_version   = "4.0"
+    provider_name           = "TENANT"
+    backing_provider        = "${var.atlas_provider}"
+    region                  = "${var.atlas_region}"
+    size                    = "M0"
+    backup                  = false
+    disk_gb_enabled         = false
+    disk_size_gb            = "0.5"
+}
