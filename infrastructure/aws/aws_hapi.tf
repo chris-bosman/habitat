e260dafd5abfe5dd1bee78fc02d095279f0c9ca9
Chris Bosman <christopher.r.bosman@gmail.com>
2019-05-08 09:43:45 -0500

    updating solution stack

--- infrastructure/aws/aws_hapi.tf
+++ infrastructure/aws/aws_hapi.tf
@@ -53,7 +53,7 @@ resource "aws_elastic_beanstalk_application" "deployment" {
 resource "aws_elastic_beanstalk_environment" "deployment" {
     name                = "${var.ORG_NAME}-ebenv-${var.ENV}-${var.REGION}"
     application         = "${aws_elastic_beanstalk_application.deployment.name}"
-    solution_stack_name = "64bit Amazon Linux 2018.03 v4.8.2 running Node.js"
+    solution_stack_name = "64bit Amazon Linux 2018.03 v4.8.3 running Node.js"
 
     setting {
         namespace   = "aws:autoscaling:launchconfiguration"
